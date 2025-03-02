136cb44acb02ebca5ebc4c2b205fd67ea8ed554c
sureshnenavath <suryanss143@gmail.com>
2025-03-02 19:29:06 +0000

    Alt attribute added to Img element

--- src/components/ReviewsCarousel/index.js
+++ src/components/ReviewsCarousel/index.js
@@ -32,39 +32,33 @@ class ReviewsCarousel extends Component {
 
   render() {
     const {activeCarouselIndex} = this.state
+    const currentReview = this.reviewsList[activeCarouselIndex]
+
     return (
       <div className="main-container">
         <h1>Reviews</h1>
         <div className="container">
-          <button>
+          <button data-testid="leftArrow" onClick={this.onHandleLeftArrowClick}>
             <img
               src="https://assets.ccbp.in/frontend/react-js/left-arrow-img.png"
               alt="left arrow"
               className="arrow-icon"
-              onClick={this.onHandleLeftArrowClick}
             />
           </button>
           <div className="carousel-container">
-            <img
-              src={this.reviewsList[activeCarouselIndex].imgUrl}
-              alt={this.reviewsList[activeCarouselIndex].username}
-            />
-            <p className="align-center">
-              {this.reviewsList[activeCarouselIndex].username}
-            </p>
-            <p className="align-center">
-              {this.reviewsList[activeCarouselIndex].companyName}
-            </p>
-            <p className="align-center">
-              {this.reviewsList[activeCarouselIndex].description}
-            </p>
+            <img src={currentReview.imgUrl} alt={currentReview.username} />
+            <p className="align-center">{currentReview.username}</p>
+            <p className="align-center">{currentReview.companyName}</p>
+            <p className="align-center">{currentReview.description}</p>
           </div>
-          <button>
+          <button
+            data-testid="rightArrow"
+            onClick={this.onHandleRightArrowClick}
+          >
             <img
               src="https://assets.ccbp.in/frontend/react-js/right-arrow-img.png"
               alt="right arrow"
               className="arrow-icon"
-              onClick={this.onHandleRightArrowClick}
             />
           </button>
         </div>
